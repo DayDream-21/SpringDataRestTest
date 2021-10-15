a5a0bd429198ab43171fe853dc57d3046fe4b4d8
DayDream-21 <vova13158@gmail.com>
2021-10-15 17:24:32 +0300

    Initial commit

---
+++ src/main/java/com/slavamashkov/spring/springboot/spring_data_rest/SpringDataRestTestApplication.java
@@ -0,0 +1,13 @@
+package com.slavamashkov.spring.springboot.spring_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slavamashkov/spring/springboot/spring_data_rest/SpringDataRestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slavamashkov.spring.springboot.spring_data_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
